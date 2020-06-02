8f32d5f1e6f3ade98644b5ef7e26a855292db0d1
Ray-03 <richard.rinaldy@binus.ac.id>
2020-06-02 14:05:20 +0700

    create animated text for logo

--- qr_code/lib/welcome_page.dart
+++ qr_code/lib/welcome_page.dart
@@ -13,10 +13,18 @@ class _WelcomePageState extends State<WelcomePage> {
     return SafeArea(
       child: Scaffold(
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             TypewriterAnimatedTextKit(
               text: ['QR_Profiler'],
               speed: Duration(milliseconds: 300),
+              textAlign: TextAlign.center,
+              textStyle: TextStyle(
+                color: Colors.tealAccent,
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
