87d415c429ae2142139dd6ecef630bb6f1a361c2
Ray-03 <richard.rinaldy@binus.ac.id>
2020-06-02 08:26:04 +0700

    add welcome page add welcome page id

---
+++ qr_code/lib/welcome_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  static const String id = 'welcome_page';
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
