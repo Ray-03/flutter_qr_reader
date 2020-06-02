822a5591d6ea7328455bf8f0bddf946485af91d4
Ray-03 <richard.rinaldy@binus.ac.id>
2020-06-02 07:19:34 +0700

    delete column (unused widget)

--- qr_code/lib/generate_page.dart
+++ qr_code/lib/generate_page.dart
@@ -38,82 +38,78 @@ class _GeneratePageState extends State<GeneratePage> {
             ),
             Container(
               padding: EdgeInsets.all(75),
-              child: Column(
-                children: [
-                  Center(
-                    child: Stack(
-                      alignment: Alignment(0, 0),
-                      children: [
-                        Container(
-                          width: 198,
-                          height: 198,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            gradient: LinearGradient(
-                              colors: [Colors.blueAccent, Colors.blue],
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                              stops: [0, 1],
-                            ),
-                          ),
+              child: Center(
+                child: Stack(
+                  alignment: Alignment(0, 0),
+                  children: [
+                    Container(
+                      width: 198,
+                      height: 198,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        gradient: LinearGradient(
+                          colors: [Colors.blueAccent, Colors.blue],
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          stops: [0, 1],
                         ),
-                        Container(
-                          width: 189,
-                          height: 189,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: Colors.white,
-                          ),
-                        ),
-                        QrImage(
-                          data: value,
-                          size: 198,
-                          version: QrVersions.auto,
-                          gapless: true,
-                          constrainErrorBounds: true,
-                          embeddedImageStyle:
-                              QrEmbeddedImageStyle(size: Size(80, 80)),
-                          errorCorrectionLevel: QrErrorCorrectLevel.Q,
-                          errorStateBuilder: (context, err) {
-                            return Container(
-                              child: Text(
-                                'ummm... something went wrong',
-                                textAlign: TextAlign.center,
-                              ),
-                            );
-                          },
-                        ),
-                        Container(
-                          width: 64,
-                          height: 64,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            color: Colors.white,
-                          ),
-                        ),
-                        Container(
-                          width: 53,
-                          height: 53,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            gradient: LinearGradient(
-                                colors: [
-                                  Colors.cyan.shade100,
-                                  Colors.lightBlueAccent.shade100
-                                ],
-                                stops: [
-                                  0,
-                                  1
-                                ],
-                                begin: Alignment.topRight,
-                                end: Alignment.bottomLeft),
-                          ),
-                          child: Image.asset('images/logoBINUS.png'),
-                        )
-                      ],
+                      ),
                     ),
-                  ),
-                ],
+                    Container(
+                      width: 189,
+                      height: 189,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+                      ),
+                    ),
+                    QrImage(
+                      data: value,
+                      size: 198,
+                      version: QrVersions.auto,
+                      gapless: true,
+                      constrainErrorBounds: true,
+                      embeddedImageStyle:
+                          QrEmbeddedImageStyle(size: Size(80, 80)),
+                      errorCorrectionLevel: QrErrorCorrectLevel.Q,
+                      errorStateBuilder: (context, err) {
+                        return Container(
+                          child: Text(
+                            'ummm... something went wrong',
+                            textAlign: TextAlign.center,
+                          ),
+                        );
+                      },
+                    ),
+                    Container(
+                      width: 64,
+                      height: 64,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.white,
+                      ),
+                    ),
+                    Container(
+                      width: 53,
+                      height: 53,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.cyan.shade100,
+                              Colors.lightBlueAccent.shade100
+                            ],
+                            stops: [
+                              0,
+                              1
+                            ],
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft),
+                      ),
+                      child: Image.asset('images/logoBINUS.png'),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
